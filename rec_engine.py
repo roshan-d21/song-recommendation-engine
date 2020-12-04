8719afcc454a6627246a970aad46a1064b864162
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-12-05 00:49:59 +0530

    Update engine

--- rec_engine.py
+++ rec_engine.py
@@ -1,20 +1,28 @@
+from pyspark.sql.session import SparkSession
+from pyspark.context import SparkContext
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
 
-plays_df_schema = StructType(
-  [StructField('userId', StringType()),
-   StructField('songId', StringType()),
-   StructField('Plays', IntegerType())]
-)
+# sc = SparkSession.builder.master("local").appName("Recommender").getOrCreate()
+# spark = sc.sparkContext
 
-metadata_df_schema = StructType(
-   [StructField('songId', StringType()),
-   StructField('title', StringType()),
-   StructField('release', StringType()),
-   StructField('artist_name', StringType()),
-   StructField('year', IntegerType())]
-)
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+
+plays_df_schema = StructType([
+    StructField('userId', StringType()),
+    StructField('songId', StringType()),
+    StructField('Plays', IntegerType())
+])
+
+metadata_df_schema = StructType([
+    StructField('songId', StringType()),
+    StructField('title', StringType()),
+    StructField('release', StringType()),
+    StructField('artist_name', StringType()),
+    StructField('year', IntegerType())
+])
 
 userdata = spark.read.format('csv') \
     .options(delimiter='\t', header=False, inferSchema=False) \
@@ -26,7 +34,8 @@ metadata = spark.read.format('csv') \
     .schema(metadata_df_schema) \
     .load('song_data.csv')
 
-# userdata = userdata.filter(userdata.Plays > 1)
+
+# Convert userID and songID to Integers
 
 i = 0
 def id_gen(value):
@@ -36,10 +45,10 @@ def id_gen(value):
 
 id_func = F.udf(id_gen, IntegerType())
 
-userId_change = userdata.select('userId').distinct().withColumn('new_userId', id_func("userId")) #.select('userId', F.monotonically_increasing_id().alias('new_userId'))
+userId_change = userdata.select('userId').distinct().withColumn('new_userId', id_func("userId"))
 
 i = 0
-songId_change = userdata.select('songId').distinct().withColumn('new_songId', id_func("songId")) #.select('songId', F.monotonically_increasing_id().alias('new_songId'))
+songId_change = userdata.select('songId').distinct().withColumn('new_songId', id_func("songId"))
 
 unique_users = userId_change.count()
 unique_songs = songId_change.count()
@@ -53,17 +62,20 @@ rdd = userdata.rdd.map(tuple).map(lambda x: (int(x[3]), int(x[4]), int(x[2])))
 
 model = ALS.train(rdd, 10, 5)
 
+# print('Enter a user id to show personalized recommendations:')
+# UserID = int(input())
+
 UserID = 13
 listened_songs = userdata.filter(userdata.new_userId == UserID) \
                         .join(metadata, 'songId') \
                         .select('new_songId', 'artist_name', 'title') \
-                                          
+                                        
 # generate list of listened songs
 listened_songs_list = []
 for song in listened_songs.collect():
-  listened_songs_list.append(song['new_songId'])
+    listened_songs_list.append(song['new_songId'])
 
-print('Songs user has listened to:')
+print('Songs User 13 has listened to:')
 listened_songs.select('artist_name', 'title').show()
 
 # generate dataframe of unlistened songs
@@ -77,9 +89,10 @@ predicted_listens_rdd = model.predictAll(unlistened_songs_rdd)
 
 predicted_listens = predicted_listens_rdd.toDF(['new_userId', 'new_songId', 'rating'])
 
+print('Songs recommended by us:')
 predicted_listens.join(userdata, 'new_songId') \
-                 .join(metadata, 'songId') \
-                 .select('artist_name', 'title', 'rating') \
-                 .distinct() \
-                 .orderBy('rating', ascending = False) \
-                 .show(10)
+                .join(metadata, 'songId') \
+                .select('artist_name', 'title', 'rating') \
+                .distinct() \
+                .orderBy('rating', ascending = False) \
+                .show(10)
